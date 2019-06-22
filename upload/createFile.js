c5c3f60aaecc989a3f3db471dd8021183ccc173e
sebastianserrano <juancho_sss@hotmail.com>
2019-06-22 13:12:19 -0400

    CREATE FILE

---
+++ upload/createFile.js
@@ -0,0 +1,36 @@
+const sha1 = require('js-sha1');
+const moment = require('moment');
+const fileType = require('file-type');
+const File = require('./File.js');
+
+function createFileFromBuffer(buffer) {
+  const filePath = createFilePath();
+  const fileName = createFileNameFromCurrentDate();
+  const fileMime = fileType(buffer);
+  const fileExtension = createFileExtensionFromMime(fileMime);
+
+  const fileFullName = filePath + fileName + fileExtension;
+  const file = new File(fileName, fileFullName, fileMime.mime)
+
+  return file;
+}
+
+function createFileExtensionFromMime(mime){
+  return '.' + mime.ext;
+}
+
+function createFilePath(){
+  const hash = createHash();
+
+  return hash + '/';
+}
+
+function createHash(){
+  return sha1(new Date().toString());
+}
+
+function createFileNameFromCurrentDate(){
+  return moment().format('YYYY-MM-DD-HH:mm:ss');
+}
+
+module.exports = createFileFromBuffer;
